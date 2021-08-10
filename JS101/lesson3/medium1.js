60a573256cbf776ad5a0956f54189000c97dd014
Tyler Wenzel <Tyler@Tylers-MBP.lan1>
2021-08-10 19:46:10 -0400

    completed medium1 as well as hard problems

--- JS101/lesson3/medium1.js
+++ JS101/lesson3/medium1.js
@@ -27,3 +27,66 @@ function factors(number) {
   }
 }
 console.log(factors(4));
+
+//Q4
+/*
+Push is a destructive method, or it changes the orginal object and concat is a
+non-destructive and will return a new string
+*/
+
+//Q5
+/*
+1) will return 0.9 /// 0.899999999999
+2)true // false
+*/
+
+//Q6
+//False
+//.isNan
+
+//Q7
+//34
+
+//Q8
+//Yes
+
+//Q9
+//paper
+
+//Q10
+//"no"
+
+//Hard problem 3
+//A
+//one is one
+// two is two
+// three is three
+
+//B
+//one is one
+//two is two
+//three is three
+
+//C
+//one is two
+//two is three
+//three is one
+
+//Q4
+
+
+function isDotSeparatedIpAddress(inputString) {
+  let dotSeparatedWords = inputString.split(".");
+  if (dotSeparatedWords.legnth !== 4) {
+    return false;
+  } else {
+    while (dotSeparatedWords.length > 0) {
+      let word = dotSeparatedWords.pop();
+      if (!isAnIpNumber(word)) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
