66913bba093b312ab85c5b283e842c7d5e816990
Tyler Wenzel <tyler.gh3@gmail.com>
2022-01-25 14:43:24 -0500

    refactored calculator

--- JS101_2022/lesson_2/calculator.js
+++ JS101_2022/lesson_2/calculator.js
@@ -1,30 +1,60 @@
 const readline = require('readline-sync');
 
-console.log('Welcome to Calculator!');
+
+function invalidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
+
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+prompt('Welcome to Calculator!');
 
 // Ask the user for the first number.
-console.log("What's the first number?");
+prompt("What's the first number?");
 let number1 = readline.question();
 
+while (invalidNumber(number1)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  number1 = readline.question();
+}
+
 // Ask the user for the second number.
-console.log("What's the second number?");
+prompt("What's the second number?");
 let number2 = readline.question();
 
+while (invalidNumber(number2)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  number2 = readline.question();
+}
+
 // Ask the user for an operation to perform.
-console.log('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
+prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
 let operation = readline.question();
+
+while (!['1', '2', '3', '4'].includes(operation)) {
+  prompt('Must choose 1, 2, 3 or 4');
+  operation = readline.question();
+}
+
 
 // Perform the operation on the two numbers.
 let output;
-if (operation === '1') {
-  output = Number(number1) + Number(number2);
-} else if (operation === '2') {
-  output = Number(number1) - Number(number2);
-} else if (operation === '3') {
-  output = Number(number1) * Number(number2);
-} else if (operation === '4') {
-  output = Number(number1) / Number(number2);
+switch (operation) {
+  case '1':
+    output = Number(number1) + Number(number2);
+    break;
+  case '2':
+    output = Number(number1) - Number(number2);
+    break;
+  case '3':
+    output = Number(number1) * Number(number2);
+    break;
+  case '4':
+    output = Number(number1) / Number(number2);
+    break;
 }
 
 // Print the result to the terminal.
-console.log(`The result is: ${output}`);
+prompt(`The result is: ${output}`);
