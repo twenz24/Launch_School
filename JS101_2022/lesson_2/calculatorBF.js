76440ba1cefc31a0cd18470211f8b728fd536f71
Tyler Wenzel <tyler.gh3@gmail.com>
2022-01-25 17:47:53 -0500

    calculator with added features

---
+++ JS101_2022/lesson_2/calculatorBF.js
@@ -0,0 +1,71 @@
+const readline = require('readline-sync');
+
+
+function invalidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
+
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+while (true) {
+
+  prompt('Welcome to Calculator!');
+
+  // Ask the user for the first number.
+  prompt("What's the first number?");
+  let number1 = readline.question();
+
+  while (invalidNumber(number1)) {
+    prompt("Hmm... that doesn't look like a valid number.");
+    number1 = readline.question();
+  }
+
+  // Ask the user for the second number.
+  prompt("What's the second number?");
+  let number2 = readline.question();
+
+  while (invalidNumber(number2)) {
+    prompt("Hmm... that doesn't look like a valid number.");
+    number2 = readline.question();
+  }
+
+  // Ask the user for an operation to perform.
+  prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
+  let operation = readline.question();
+
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    prompt('Must choose 1, 2, 3 or 4');
+    operation = readline.question();
+  }
+
+
+  // Perform the operation on the two numbers.
+  let output;
+  switch (operation) {
+    case '1':
+      output = Number(number1) + Number(number2);
+      break;
+    case '2':
+      output = Number(number1) - Number(number2);
+      break;
+    case '3':
+      output = Number(number1) * Number(number2);
+      break;
+    case '4':
+      output = Number(number1) / Number(number2);
+      break;
+  }
+
+  // Print the result to the terminal.
+  prompt(`The result is: ${output}`);
+
+  prompt('Would you like perform another calculation? (yes or no)');
+  let performAgain = readline.question();
+
+  if (performAgain[0].toLowerCase() === 'n') {
+    prompt('Thank you for using Calculator!');
+    break;
+  }
+}
