732c2213c877aea8e092690629d170780fc69faa
Tyler Wenzel <tyler.gh3@gmail.com>
2022-02-14 17:04:32 -0500

    adding assessment prep folder

---
+++ JS101_2022/assessment_prep/written/codedescriptions.js
@@ -0,0 +1,33 @@
+/*
+
+#1
+// for loop transformation
+let numbers = [1, 2, 3, 4, 5];
+let transformedNumbers = [];
+
+for (let index = 0; index < numbers.length; index += 1) {
+  let currentNum = numbers[index];
+  let squaredNum = currentNum * currentNum;
+
+  transformedNumbers.push(squaredNum);
+}
+
+transformedNumbers; // => [ 1, 4, 9, 16, 25 ]
+
+Description
+On line five we declare and intialize the global variable "numbers" to the
+array object [1, 2, 3, 4, 5]. Additionally on line 6, we declare and intialize
+the global transformedNumbers to an empty array. on line 8 we use a for 
+loop to iterate over the numbers array adding 1 to the index for each iteration
+stopping the when the index is = to the number array's length.
+During each iteration we are declaring and initalizing the local variable
+currentNumber to the element in the number array at index.
+We then declare and initalize the local variable squaredNum and set it equal to
+currentNum * currentNum
+
+then we mutate transformedNumbers array by adding(pushing) the squareNum as an
+element
+
+the result is an array with all elements of the numbers array squared.
+
+*/
---
+++ JS101_2022/assessment_prep/written/misc.js
@@ -0,0 +1,3 @@
+let result = [1, 2, 3, 4].find(a => a < 3);
+
+console.log(result);
